05e0175cd1cb3270c4efbdb6c91ae0c5690a37c5
pmaganach <pablo.magana@verisure.cl>
2026-05-23 22:03:22 -0400

    revert: quitar sort del frontend, el orden va en el backend

--- frontend/src/api/usuarios.js
+++ frontend/src/api/usuarios.js
@@ -1,5 +1,3 @@
 import client from './client'
 
-export const getUsuarios = () => client.get('/usuarios/').then(r =>
-  r.data.sort((a, b) => a.nombre.localeCompare(b.nombre, 'es'))
-)
+export const getUsuarios = () => client.get('/usuarios/').then(r => r.data)
